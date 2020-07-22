359f042118aa8e7bf28d41ae36692458770e9ab6
Dennis <beiteldennis123@gmail.com>
2020-07-22 02:55:17 +0200

    movies

---
+++ src/SearchMovies.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import MovieCard from './MovieCard';
+import { MOVIE_API_KEY } from './keys.js';
+
+export default function SearchMovies() {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label htmlFor="query" className="label">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          value={query}
+          placeholder="i.e. Harry Potter"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </>
+  );
+}
